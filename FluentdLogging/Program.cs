0cdc63f2c997c6146d0d66e78ffffdb263199eac
Mustafa Korkmaz <m.korkmaz@outlook.com>
2022-11-22 00:09:19 +0200

    Add project files.

---
+++ FluentdLogging/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder
+                .AddFilter("Microsoft", LogLevel.Warning)
+                .AddFilter("System", LogLevel.Warning)
+                .AddFilter("Program", LogLevel.Trace)
+                .AddConsole();
+        });
+
+        ILogger logger = loggerFactory.CreateLogger<Program>();
+
+        logger.LogTrace("A trace level log");
+
+        logger.LogInformation("An information level log");
+
+        Console.WriteLine("A Console.WriteLine log");
+    }
+}
